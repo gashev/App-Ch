ffa3259bae1988954538ac5e8e3aed0262c12cb7
Oleg Gashev <oleg@gashev.net>
2013-09-12 19:50:11 +0000

    Added setup options.

--- lib/App/Ch/Command/setup.pm
+++ lib/App/Ch/Command/setup.pm
@@ -5,6 +5,14 @@ use warnings;
 
 use App::Ch -command;
 
+sub opt_spec {
+    return (
+        [ 'arch',  'set the target architecture [ --arch=i386 ]' ],
+        [ 'distribution', 'distribution [ sid|experimental|... ]'],
+        [ 'mirror',  'mirror location [ http://ftp.us.debian.org/debian ]' ],
+    );
+}
+
 sub execute {
     my ($self, $opt, $args) = @_;
 
