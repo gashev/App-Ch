cf2e7b8619dc80641d35c3c904ef5d8121204918
Oleg Gashev <oleg@gashev.net>
2013-09-12 21:05:59 +0000

    Set default values to setup commands.

--- lib/App/Ch/Command/setup.pm
+++ lib/App/Ch/Command/setup.pm
@@ -7,9 +7,21 @@ use App::Ch -command;
 
 sub opt_spec {
     return (
-        [ 'arch',  'set the target architecture [ --arch=i386 ]' ],
-        [ 'distribution', 'distribution [ sid|experimental|... ]'],
-        [ 'mirror',  'mirror location [ http://ftp.us.debian.org/debian ]' ],
+        [
+            'arch=s',
+            'set the target architecture [ --arch=i386 ]',
+            { default => 'i386' }
+        ],
+        [
+            'distribution=s',
+            'distribution [ sid|experimental|... ]' ,
+            { default => 'sid' }
+        ],
+        [
+            'mirror=s',
+            'mirror location [ http://ftp.us.debian.org/debian ]',
+            { default => 'http://ftp.us.debian.org/debian' }
+        ],
     );
 }
 
