ac7919223416160846b4537555d6c55caa7b0b41
Oleg Gashev <oleg@gashev.net>
2013-09-13 03:43:28 +0000

    Added delete command.

---
+++ lib/App/Ch/Command/delete.pm
@@ -0,0 +1,25 @@
+package App::Ch::Command::delete;
+
+use strict;
+use warnings;
+
+use App::Ch -command;
+
+sub opt_spec {
+    return (
+        [
+            'name=s',
+            'name'
+        ]
+    );
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+
+    my $name = $opt->name;
+
+    exec("rm -rf /opt/chroot/$name");
+}
+
+1;
