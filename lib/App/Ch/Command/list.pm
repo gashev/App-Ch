b27ac2270a5349745a230ee3a755b7703d56d42d
Oleg Gashev <oleg@gashev.net>
2013-09-20 18:05:00 +0000

    Added pod to list and login commands.

--- lib/App/Ch/Command/list.pm
+++ lib/App/Ch/Command/list.pm
@@ -20,3 +20,35 @@ sub execute {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 SYNOPSIS
+
+  ch --root=CH_REPOSITORY_ROOT list
+
+=head1 DESCRIPTION
+
+This command lists the chroots that are registered on the repository.
+
+=head1 COMMAND ARGUMENTS
+
+None.
+
+=head1 AUTHOR
+
+Oleg Gashev <oleg@gashev.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Oleg Gashev.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
--- lib/App/Ch/Command/login.pm
+++ lib/App/Ch/Command/login.pm
@@ -26,3 +26,36 @@ sub execute {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 SYNOPSIS
+
+  ch --root=CH_REPOSITORY_ROOT login TARGET
+
+=head1 DESCRIPTION
+
+Logs into the chroot. Only use this after mount filesystems directly inside the
+chroot. For mount filesystems see start command.
+
+=head1 COMMAND ARGUMENTS
+
+Argument is the chroot name.
+
+=head1 AUTHOR
+
+Oleg Gashev <oleg@gashev.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Oleg Gashev.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
